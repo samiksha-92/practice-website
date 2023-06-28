88f2bd231e211f4ffb68ab90c52f1a947a778421
samiksha-92 <38665362+samiksha-92@users.noreply.github.com>
2023-06-28 11:47:02 +0000

    rendering jobs and location
    Displaying open job positions and their respective locations

--- app.py
+++ app.py
@@ -2,10 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': "Data Analyst",
+    'location': "Bangalore,India",
+    'Salary': "Rs.10,00,000"
+  },
+  {
+    'id': 2,
+    'title': "Front End Engineer",
+    'location': "Fully Remote",
+    'Salary': "Rs.20,00,000"
+  },
+  {
+    'id': 3,
+    'title': "Director of Marketing",
+    'location': "Newyork, US",
+    'Salary': "$10,00,000"
+  },
+  {
+    'id': 4,
+    'title': "Community Manager",
+    'location': "San Francisco, US",
+    'Salary': "$60,000"
+  }
+  
+]
+
 
 @app.route("/")
 def sam_title():
-  return render_template('home.html')
+  return render_template('home.html', jobs= JOBS)
 
 
 if __name__ == "__main__":
